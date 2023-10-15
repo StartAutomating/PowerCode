d5645bb4f4d92a086677a93a881251b4b11a22bd
James Brundage <@github.com>
2023-10-15 14:19:28 -0700

    GetProductInfo (Fixes #23)

---
+++ Extensions/GetProductInfo.PowerCode.ps1
@@ -0,0 +1,27 @@
+<#
+.SYNOPSIS
+    Gets the Product Information
+.DESCRIPTION
+    Gets the Visual Studio Code Product Information stored in product.json
+.EXAMPLE
+    Get-VSCode -ProductInfo
+#>
+[Management.Automation.Cmdlet("Get","VSCode")]
+param(
+# If set, will get the version of VSCode.
+[Parameter(Mandatory)]
+[switch]
+$ProductInfo
+)
+
+
+Get-Command -CommandType Application -Name code |
+    Select-Object -ExpandProperty Source | 
+    Split-Path |
+    Split-Path |
+    Get-ChildItem -Recurse -Filter product.json |
+    Select-Object -First 1 |    
+    Get-Content -Raw -LiteralPath { $_.FullName } |
+    ConvertFrom-Json
+
+
