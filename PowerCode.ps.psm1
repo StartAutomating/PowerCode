ff980826e16733a3ebc82a45079888554b94f0e4
James Brundage <@github.com>
2023-10-15 11:09:10 -0700

    PowerCode module - Exporting $PowerCode (Fixes #16)

--- PowerCode.ps.psm1
+++ PowerCode.ps.psm1
@@ -1,1 +1,7 @@
 [Include('*-*.ps1')]$PSScriptRoot
+
+$PowerCode = $this = $MyInvocation.MyCommand.ScriptBlock.Module
+
+$PowerCode.pstypenames.insert(0, 'PowerCode')
+
+Export-ModuleMember -Function * -Alias * -Variable PowerCode
