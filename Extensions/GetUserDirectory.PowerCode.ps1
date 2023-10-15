eeeeb7b7c1aad01cf60195010924e630f8ccd0a9
James Brundage <@github.com>
2023-10-15 11:56:22 -0700

    PowerCode.GetUserDirectory (Fixes #37)

---
+++ Extensions/GetUserDirectory.PowerCode.ps1
@@ -0,0 +1,38 @@
+<#
+.SYNOPSIS
+    Gets the VSCode user directory
+.DESCRIPTION
+    Gets the Visual Studio Code user directory.
+.EXAMPLE
+    Get-PowerCode -GetUserDirectory
+.EXAMPLE
+    PowerCode -GetUserDirectory
+#>
+[Management.Automation.Cmdlet("(?:Get){0,1}","(?>Power|VS)Code")]
+param(
+# If set, will get the user directory.
+[Parameter(Mandatory)]
+[switch]
+$GetUserDirectory
+)
+
+if (-not $script:VSCodeUserDirectory) {
+    $script:VSCodeUserDirectory = 
+        if ($IsMacOS) 
+        {
+            "$(Join-Path $home Library |
+                Join-Path -ChildPath 'Application\ Support' |
+                Join-Path -ChildPath Code |
+                Join-Path -ChildPath User)"                    
+        } 
+        elseif ($IsLinux) 
+        {
+            "$HOME/.config/Code/User/"
+        } else {
+            "$env:APPDATA\Code\User\"
+        }                
+}
+
+if (Test-path $script:VSCodeUserDirectory) {
+    Get-Item -Path $script:VSCodeUserDirectory
+}
