229b393363471bc8aabb6c801c9a3ccfe872aeca
James Brundage <@github.com>
2023-10-15 15:10:03 -0700

    Adding GetUserKeybindings (Fixes #48)

---
+++ Extensions/GetUserKeyBindings.PowerCode.ps1
@@ -0,0 +1,31 @@
+<#
+.SYNOPSIS
+    Gets VSCode user KeyBinding
+.DESCRIPTION
+    Gets the user KeyBindings from Visual Studio Code
+.EXAMPLE
+    Get-PowerCode -GetUserKeyBinding
+.EXAMPLE
+    PowerCode -GetUserKeyBindings
+#>
+[Management.Automation.Cmdlet("(?:Get){0,1}","(?>Power|VS)Code")]
+param(
+# If set, will get the current user's KeyBindings.
+[Parameter(Mandatory)]
+[Alias('GetUserKeyBinding')]
+[switch]
+$GetUserKeyBindings
+)
+
+
+PowerCode -GetUserDirectory |
+    Get-ChildItem -Filter keybindings.json |
+    & { process {
+        [IO.File]::ReadAllText($_.Fullname) | 
+        ConvertFrom-Json |
+        ForEach-Object {
+            $vsCodeKeybinding = $_
+            $vsCodeKeybinding.pstypenames.insert(0, 'VSCode.Keybinding')
+            $vsCodeKeybinding            
+        }
+    } }
