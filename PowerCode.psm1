3b9d8a84e9a91f6813af192aafbf661513ad3857
StartAutomating <StartAutomating@github.com>
2023-10-15 18:13:05 +0000

    PowerCode module - Exporting $PowerCode (Fixes #16)
    Temporarily not importing types

--- PowerCode.psm1
+++ PowerCode.psm1
@@ -1,5 +1,16 @@
-foreach ($file in (Get-ChildItem -Path "$PSScriptRoot" -Filter "*-*.ps1" -Recurse)) {
+:ToIncludeFiles foreach ($file in (Get-ChildItem -Path "$PSScriptRoot" -Filter "*-*.ps1" -Recurse)) {
     if ($file.Extension -ne '.ps1')      { continue }  # Skip if the extension is not .ps1
-    if ($file.Name -match '\.[^\.]+\.ps1$') { continue }  # Skip if the file is an unrelated file.
+    foreach ($exclusion in '\.[^\.]+\.ps1$') {
+        if (-not $exclusion) { continue }
+        if ($file.Name -match $exclusion) {
+            continue ToIncludeFiles  # Skip excluded files
+        }
+    }     
     . $file.FullName
 }
+
+$PowerCode = $this = $MyInvocation.MyCommand.ScriptBlock.Module
+
+$PowerCode.pstypenames.insert(0, 'PowerCode')
+
+Export-ModuleMember -Function * -Alias * -Variable PowerCode
