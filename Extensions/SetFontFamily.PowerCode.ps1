90d79c9b991057e97c21737ca604b777cd855a5f
James Brundage <@github.com>
2023-10-15 15:17:45 -0700

    Adding SetFontFamily (Fixes #46)
    Also changing SetFontSize to avoid conflicts

--- Extensions/SetFontFamily.PowerCode.ps1
+++ Extensions/SetFontFamily.PowerCode.ps1
@@ -1,29 +1,29 @@
 <#
 .SYNOPSIS
-    Sets the VS Code Font Size
+    Sets the VS Code Font Family
 .DESCRIPTION
-    Sets the Visual Studio Code Font Size
+    Sets the Visual Studio Code Font Family
 .EXAMPLE
-    Set-PowerCode -SetFontSize 24
+    Set-PowerCode -SetFontFamily "Consolas"
 .EXAMPLE
-    Set-PowerCode -SetFontSize 48 -SetFontIn screencast
+    Set-PowerCode -SetFontFamily "Consolas" -SetFontIn terminal
 .EXAMPLE
-    Update-PowerCode -UpdateFontSize 16 -SetFontIn markdown
+    Update-PowerCode -UpdateFontFamily 16 -SetFontFamily "Arial"
 #>
 [Management.Automation.Cmdlet("(?>Update|Set)","(?>Power|VSCode)")]
 param(
 # If set, will change the font size
 [Parameter(Mandatory,ValueFromPipelineByPropertyName)]
-[Alias('UpdateFontSize')]
-[double]
-$SetFontSize,
+[Alias('UpdateFontFamily')]
+[string]
+$SetFontFamily,
 
 # Where the font size change should take place.
 [ValidateSet('editor','terminal','screencast','markdown')]
 [Parameter(ValueFromPipelineByPropertyName)]
-[Alias('UpdateFontSizeIn')]
+[Alias('UpdateFontFamilyIn')]
 [string[]]
-$SetFontSizeIn = @('editor','terminal')
+$SetFontFamilyIn = @('editor','terminal')
 )
 
 $lookupSetIn = @{
@@ -32,12 +32,12 @@ $lookupSetIn = @{
     "markdown" = "markdown.preview"
 }
 
-foreach ($setIn in $SetFontIn) {
+foreach ($setIn in $SetFontFamilyIn) {
     $lookedUpSet =  if ($lookupSetIn[$setIn]) {
         $lookupSetIn[$setIn]
-    } else { $setIn }    
+    } else { $setIn.ToLower() }    
     Set-VSCode -Change @{
-        "$($lookedUpSet).fontSize" = $SetFontSize
+        "$($lookedUpSet).fontFamily" = $SetFontFamily
     }
 }
 
