16011f8f880e110a5633de334241bde97d5a9018
James Brundage <@github.com>
2023-10-15 12:25:45 -0700

    GetPackageFileList (Fixes #41)

---
+++ Extensions/GetPackageFileList.PowerCode.ps1
@@ -0,0 +1,21 @@
+<#
+.SYNOPSIS
+    Gets the list of package files
+.DESCRIPTION
+    Gets the list of package files that power Visual Studio Code.
+.EXAMPLE
+    Get-PowerCode -PackageFileList
+#>
+[Management.Automation.Cmdlet("(?:Get)?","(?>Power|VS)Code")]
+param(
+# If set, will get a list of all package files.
+[Parameter(Mandatory)]
+[switch]
+$PackageFileList
+)
+
+Get-Command -CommandType Application -Name code |
+    Select-Object -ExpandProperty Source | 
+    Split-Path |
+    Split-Path |
+    Get-ChildItem -Recurse -Filter package.json
