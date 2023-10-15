705535c82a921800e8636bf0195ec32dc92389e2
James Brundage <@github.com>
2023-10-15 13:40:25 -0700

    GetCodeCommand (Fixes #42)

---
+++ Extensions/GetCodeCommand.PowerCode.ps1
@@ -0,0 +1,35 @@
+<#
+.SYNOPSIS
+    Gets the VS Code Commands
+.DESCRIPTION
+    Gets the Visual Studio Code command list
+.EXAMPLE
+    Get-PowerCode -CommandList    
+.EXAMPLE
+    Get-VSCode -CommandList
+#>
+[Management.Automation.Cmdlet("(?:Get){0,1}","(?>Power|VS)Code")]
+param(
+# If set, will get the version of VSCode.
+[Parameter(Mandatory)]
+[Alias('CommandsList','ListCommand','ListCommands')]
+[switch]
+$CommandList
+)
+
+@(
+    PowerCode -PackageFileList
+    PowerCode -ExtensionPackageFileList
+) |
+    ForEach-Object {
+        $file = $_
+        $fileText = Get-Content -Raw -LiteralPath $file.Fullname
+        if ($fileText -match 'commands') {
+            $fileData =  $fileText | 
+                ConvertFrom-Json
+
+            if ($fileData.contributes.commands) {
+                $fileData.contributes.commands
+            }            
+        }        
+    }
