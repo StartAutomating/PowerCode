920ddc95f0aaa389c34d6c424c8cb0537e36bfa9
James Brundage <@github.com>
2023-10-30 23:08:12 -0700

    enhancement: GetUserTaskInputs (Fixes #67)

---
+++ Extensions/Tasks/GetUserTaskInputs.PowerCode.ps1
@@ -0,0 +1,32 @@
+<#
+.SYNOPSIS
+    Gets VSCode user task inputs
+.DESCRIPTION
+    Gets the user tasks from Visual Studio Code
+.EXAMPLE
+    Get-PowerCode -GetUserTaskInputs
+.EXAMPLE
+    PowerCode -GetUserTaskInput
+#>
+[ValidatePattern('^(?:Get-)?(?>Power|VS)Code')]
+param(
+# If set, will get the current user's tasks.
+[Parameter(Mandatory)]
+[Alias('GetUserTaskInputs')]
+[switch]
+$GetUserTaskInput
+)
+
+
+PowerCode -GetUserDirectory |
+    Get-ChildItem -Filter tasks.json |
+    & { process {
+        [IO.File]::ReadAllText($_.Fullname) | 
+        ConvertFrom-Json |
+        ForEach-Object {
+            foreach ($vsCodeTask in $_.inputs) {
+                $vsCodeTask.pstypenames.insert(0, 'VSCode.Task.Input')
+                $vsCodeTask
+            }
+        }
+    } }
